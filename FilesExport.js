0e2d6cd5316bd54078547f6122b2c325b499356d
MuraliKrishna0436 <bmuralikrishna436@gmail.com>
2024-02-20 09:50:25 +0530

    Commit for exporting the JS files

---
+++ FilesExport.js
@@ -0,0 +1,12 @@
+let name = "Murali Krishna"
+let id = 436
+
+function details(){
+     console.log("Details function...");
+}
+function test(){
+    console.log("testig the function");
+}
+module.exports = {
+    name, id, details, test
+}
